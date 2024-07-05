feeee8c93689e5f07f26fd9a48630943dbd376da
Eric Kunzel <kunzel.eric@gmail.com>
2024-07-05 08:51:39 -0300

    feat: added static css styling for chat interface

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config["SECRET_KEY"] = "SECRET_KEY_LIVE_MESSAGE"
 
 socketio = SocketIO(app)
+
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
+
 @app.route("/messages")
 def render_messages():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@socketio.on('message')
+
+@socketio.on("message")
 def handle_message(msg):
-  emit('message', msg, broadcast=True)
+    emit("message", msg, broadcast=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
